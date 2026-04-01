0d6e3c30d724859cdf1d0de9a33cdb90a8044744
Muhammad Arizky <arizkymuhammad46@gmail.com>
2026-04-01 14:30:48 +0700

    chore: update Next.js development build manifests and cache files

--- .next/dev/static/chunks/app_page_tsx_0184fu9._.js
+++ .next/dev/static/chunks/app_page_tsx_0184fu9._.js
@@ -1,5 +1,5 @@
 (globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_0vgdp~q._.js","static/chunks/node_modules_0vi2j.~._.js"],
+    chunks: ["static/chunks/_0gwgctq._.js","static/chunks/node_modules_0na.-zg._.js"],
     source: "dynamic"
 });
